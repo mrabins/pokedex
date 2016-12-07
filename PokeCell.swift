107cb9be4e71a9a86e777955bcefc7f7a4205aa1
Mark Rabins <markrabins@gmail.com>
2016-12-06 22:34:05 -0800

    Parsing pokedex

--- PokeCell.swift
+++ PokeCell.swift
@@ -23,7 +23,7 @@ class PokeCell: UICollectionViewCell {
     
     }
     
-    func configureCell(pokemon: Pokemon) {
+    func configureCell(_ pokemon: Pokemon) {
         self.pokemon = pokemon
         nameLabel.text = self.pokemon.name.capitalized
         thumbImage.image = UIImage(named: "\(self.pokemon.pokedexID)")
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -11,13 +11,46 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var collection: UICollectionView!
-
+    
+    var pokemon = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         collection.delegate = self
         collection.dataSource = self
+        
+        parsePokemonCSV()
+    }
+    
+    func parsePokemonCSV() {
+        
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
+        
+        do {
+            
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+            print(rows)
+            
+            for row in rows {
+                
+                // TODO: Handle this despite knowing the values are not nil
+                
+                let pokeID = Int(row["id"]!)!
+                let name = row["identifier"]!
+                
+                let poke = Pokemon(name: name, pokedexID: pokeID)
+                pokemon.append(poke)
+                print(pokemon.count)
+            }
+        } catch let err as NSError {
+            
+            // TODO: Handle this more gracefully
+            
+            print(err.debugDescription)
+        }
+        
     }
 }
 
@@ -30,15 +63,13 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
         
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
             
-            let pokemon = Pokemon(name: "Pokemon", pokedexID: indexPath.row)
-            cell.configureCell(pokemon: pokemon)
+            let poke = pokemon[indexPath.row]
+            cell.configureCell(poke)
             
             return cell
         } else {
             return UICollectionViewCell()
- 
         }
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -50,7 +81,8 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        print(pokemon.count)
+        return pokemon.count
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
